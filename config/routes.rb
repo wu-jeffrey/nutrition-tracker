5eb6614efaa72f18952de15b4508890e939b604a
mynameisjeffwu <jeffrey.wu@mitremedia.com>
2018-10-17 10:56:22 -0400

    🚧 remove btn from homepage, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resource :food
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
