d8d1495c90932d752fa25110e95943c7bca3a2ba
mynameisjeffwu <jeffrey.wu@mitremedia.com>
2018-10-19 21:15:15 -0400

    💄 styled list, small UI changes to add btn, input fields

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,4 @@ class Food < ActiveRecord::Base
     validates :name, presence: true,
                      length: { minimum: 2 }
     validates :cal, presence: true
-
-    validates_numericality_of :cal, :protein, :carbohydrates, :fat, :greater_than_or_equal_to => 0
 end
