f0acdd6ad8950381f70990c61da2e5c4ab528a43
mynameisjeffwu <jeffrey.wu@mitremedia.com>
2018-10-21 12:02:18 -0400

    💄style 🍻 metric totals in helper, not DRY

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,22 @@
 module HomeHelper
 
-    def no_food?(food_array)
-        return food_array.empty?
+    def no_food?(foods)
+        return foods.empty?
+    end
+    # TODO: needs to be more DRY
+    def total_calories(foods)
+        foods.inject(0) { |sum,food| sum + food.cal }
+    end
+
+    def total_protein(foods)
+        foods.inject(0) { |sum,food| sum + food.protein }
+    end
+
+    def total_carbohydrates(foods)
+        foods.inject(0) { |sum,food| sum + food.carbohydrates }
+    end
+
+    def total_fat(foods)
+        foods.inject(0) { |sum,food| sum + food.fat }
     end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ActiveRecord::Base
-    validates :name, presence: true,
-                     length: { minimum: 2 }
+    validates :name, presence: true, length: { minimum: 2 }
     validates :cal, presence: true
+
 end
