778966c03840aeb70e226f74c9a7567e098be9fc
mynameisjeffwu <jeffrey.wu@mitremedia.com>
2018-10-28 13:56:11 -0400

    💄 style fixes, remember me toggle, needs to fix remember me functionality

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,2 @@
 //  Place all the behaviors and hooks related to the matching controller here.
 //  All this logic will automatically be available in application.js.
-
-$( document ).ready(function() {
-    // $('.alert').hide();
-    // $('.notice').hide();
-});
