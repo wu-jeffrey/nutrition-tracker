992e9a1134847283ffcf84e75c8cffc1b1da4a3c
mynameisjeffwu <jeffrey.wu@mitremedia.com>
2018-11-03 18:25:57 -0400

    💄 Footer restyle, expands up, style refactor, modal scaffold

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,9 @@
 //  Place all the behaviors and hooks related to the matching controller here.
 //  All this logic will automatically be available in application.js.
+$(document).on('ready', function() {
+    $('#profile-btn').on('click', function() {
+        $('#sticky-footer').toggleClass('opened collapsed');
+        $('#settings-banner').toggleClass('hidden');
+        $('#settings-content').toggleClass('hidden');
+    });
+});
