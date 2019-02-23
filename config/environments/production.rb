782e99835630be80af1869282884b3bf88b30e89
wu-jeffrey <jeff.jiang.wu@gmail.com>
2019-02-23 13:49:20 -0500

    used my own domain instead of email domain... derp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:      ENV["MAIL_USERNAME"],
     password:       ENV["MAIL_PASSWORD"],
-    domain:         'serene-retreat-25036.herokuapp.com/',
+    domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
