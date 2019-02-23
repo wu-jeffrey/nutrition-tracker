070943592448aec1e1beeeb256e874a55c1a3f59
wu-jeffrey <jeff.jiang.wu@gmail.com>
2019-02-23 13:54:58 -0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module NutritionTracker
     # config.i18n.default_locale = :de
     # config.serve_static_assets = true
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'env.yml')
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
       end if File.exists?(env_file)
